739911d7dd2f0011d94cec32aee64dd29a140cf9
Mattias Mikkola <mattias@mikkola.me>
2017-12-05 20:40:00 +0100

    Day1

---
+++ Day1Tests/Day1Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1.Tests
+{
+    [TestClass()]
+    public class Day1Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            Assert.AreEqual(Day1.Part1("1122", 1), 3);
+            Assert.AreEqual(Day1.Part1("1111", 1), 4);
+            Assert.AreEqual(Day1.Part1("1234", 1), 0);
+            Assert.AreEqual(Day1.Part1("91212129", 1), 9);
+        }
+
+        [TestMethod()]
+        public void Part2Test()
+        {
+            Assert.AreEqual(Day1.Part2("1212"), 6);
+            Assert.AreEqual(Day1.Part2("1221"), 0);
+            Assert.AreEqual(Day1.Part2("123425"), 4);
+            Assert.AreEqual(Day1.Part2("123123"), 12);
+            Assert.AreEqual(Day1.Part2("12131415"), 4);
+        }
+
+    }
+}
