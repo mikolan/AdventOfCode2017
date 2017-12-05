9ab2e318e1d39c2a6cd4f6076de0f326d280c087
Mattias Mikkola <mattias@mikkola.me>
2017-12-05 21:05:47 +0100

    Day2

---
+++ Day2/Day2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public class Day2
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            Console.WriteLine($"Part1: {Part1(input)}");
+            Console.WriteLine($"Part2: {Part2(input)}");
+        }
+
+        public static int Part1(string[] input)
+        {
+            int checksum = 0;
+
+            foreach (var line in input)
+            {
+                int max = int.MinValue; int min = int.MaxValue;
+
+                foreach(var n in line.Split('\t').Select(s => int.Parse(s)))
+                {
+                    if(n > max)
+                        max = n;
+                    if (n < min)
+                        min = n;
+                }
+
+                checksum += max - min;
+            }
+
+            return checksum;
+        }
+
+        public static int Part2(string[] input)
+        {
+            int checksum = 0;
+
+            foreach (var line in input)
+            {
+                int[] nums = line.Split('\t').Select(s => int.Parse(s)).ToArray();
+
+
+                bool foundDivisible = false;
+
+                // Go from left to right and check i versus j (both ways)
+                for(int i = 0; i < (nums.Length - 1); i++)
+                {
+                    for(int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[i] % nums[j] == 0)
+                        {
+                            checksum += nums[i] / nums[j];
+                            foundDivisible = true;
+                            break;
+                        }
+                        else if (nums[j] % nums[i] == 0)
+                        {
+                            checksum += nums[j] / nums[i];
+                            foundDivisible = true;
+                            break;
+                        }
+                    }
+
+                    if (foundDivisible)
+                        break;
+                }
+            }
+
+            return checksum;
+        }
+    }
+}
---
+++ Day2Tests/Day2Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2.Tests
+{
+    [TestClass()]
+    public class Day2Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            string[] testInput =
+            {
+                "5\t1\t9\t5",
+                "7\t5\t3",
+                "2\t4\t6\t8"
+            };
+
+            Assert.AreEqual(Day2.Part1(testInput), 18);
+        }
+
+        [TestMethod()]
+        public void Part2Test()
+        {
+            string[] testInput =
+            {
+                "5\t9\t2\t8",
+                "9\t4\t7\t3",
+                "3\t8\t6\t5"
+            };
+
+            Assert.AreEqual(Day2.Part2(testInput), 9);
+        }
+    }
+}
