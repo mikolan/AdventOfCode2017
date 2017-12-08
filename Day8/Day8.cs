419d03e1d4e5de7ad3df79eafbf9e24fd5e48bf1
Mattias Mikkola <mattias@mikkola.me>
2017-12-08 16:59:31 +0100

    Day7

---
+++ Day8/Day8.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    public class Day8
+    {
+        private static Dictionary<string, int> registers;
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            Console.WriteLine($"Part1: {Part1(input)}");
+            Console.WriteLine($"Part2: {Part2(input)}");
+        }
+
+        public static int Part1(string[] input)
+        {
+            registers = new Dictionary<string, int>();
+
+            foreach(var line in input)
+            {
+                var cols = line.Split(' ');
+                string reg = cols[0];
+                string op = cols[1];
+                int val = int.Parse(cols[2]);
+                string x = cols[4];
+                string cond = cols[5];
+                int condVal = int.Parse(cols[6]);
+
+                InitRegisters(new[] { reg, x });
+
+                if (CheckCondition(cond, registers[x], condVal))
+                {
+                    switch (op)
+                    {
+                        case "inc":
+                            {
+                                registers[reg] += val;
+                                break;
+                            }
+                        case "dec":
+                            {
+                                registers[reg] -= val;
+                                break;
+                            }
+                        default:
+                            throw new ArgumentException($"Unknown operator: {op}");
+                    }
+                }
+            }
+
+            return registers.Values.Max();
+        }
+
+        public static int Part2(string[] input)
+        {
+            registers = new Dictionary<string, int>();
+            int globalMax = int.MinValue;
+
+            foreach (var line in input)
+            {
+                var cols = line.Split(' ');
+                string reg = cols[0];
+                string op = cols[1];
+                int val = int.Parse(cols[2]);
+                string x = cols[4];
+                string cond = cols[5];
+                int condVal = int.Parse(cols[6]);
+
+                InitRegisters(new[] { reg, x });
+
+                if (CheckCondition(cond, registers[x], condVal))
+                {
+                    switch (op)
+                    {
+                        case "inc":
+                            {
+                                registers[reg] += val;
+                                if (registers[reg] > globalMax)
+                                    globalMax = registers[reg];
+                                break;
+                            }
+                        case "dec":
+                            {
+                                registers[reg] -= val;
+                                if (registers[reg] > globalMax)
+                                    globalMax = registers[reg];
+                                break;
+                            }
+                        default:
+                            throw new ArgumentException($"Unknown operator: {op}");
+                    }
+                }
+            }
+
+            return globalMax;
+        }
+
+        private static void InitRegisters(string[] v)
+        {
+            foreach(var r in v)
+            {
+                if(!registers.ContainsKey(r))
+                {
+                    registers[r] = 0;
+                }
+            }
+        }
+
+        public static bool CheckCondition(string condition, int val1, int val2)
+        {
+            switch(condition)
+            {
+                case ">":
+                    return val1 > val2;
+                case "<":
+                    return val1 < val2;
+                case "==":
+                    return val1 == val2;
+                case ">=":
+                    return val1 >= val2;
+                case "<=":
+                    return val1 <= val2;
+                case "!=":
+                    return val1 != val2;
+                default:
+                    throw new ArgumentException($"Unknown conditional operator: {condition}");
+            }
+        }
+    }
+}
---
+++ Day8Tests/Day8Tests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            string[] input =
+            {
+                "b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10"
+            };
+
+            Assert.AreEqual(Day8.Part1(input), 1);
+        }
+
+        [TestMethod()]
+        public void Part2Test()
+        {
+            string[] input =
+            {
+                "b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10"
+            };
+
+            Assert.AreEqual(Day8.Part2(input), 10);
+        }
+    }
+}
