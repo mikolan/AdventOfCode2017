a1eeae71c37bef7f7da7d7413b7f301431667d08
Mattias Mikkola <mattias@mikkola.me>
2017-12-09 12:18:02 +0100

    Day9

---
+++ Day9/Day9.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    public class Day9
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+
+            Console.WriteLine($"Part1: {Part1(input)}");
+            //Console.WriteLine($"Part1: {Part2(input)}");
+        }
+
+        public static int Part1(string input)
+        {
+            int currentLevel = 1;
+            int score = 0;
+            TraverseGroup(input, 0, currentLevel, ref score);
+
+            return score;
+        }
+
+        private static int TraverseGroup(string input, int offset, int currentLevel, ref int currentScore)
+        {
+            int i = offset + 1;
+
+            // Traverse the string until we find another '}', which closes the group.
+            while(true)
+            {
+                if (input[i] == '}')
+                {
+                    currentScore += currentLevel;
+                    break;
+                }
+
+                if(input[i] == '<')
+                {
+                    bool foundGarbageClose = false;
+
+                    while(!foundGarbageClose)
+                    {
+                        // Find the next > and check if it's legit.
+                        i = input.IndexOf('>', i + 1);
+
+                        if (input[i - 1] == '!')
+                        {
+                            int counter = 1;
+                            int foo = i - 2;
+                            while(input[foo] == '!')
+                            {
+                                counter++;
+                                foo--;
+                            }
+
+                            if (counter % 2 == 0)
+                                foundGarbageClose = true;
+                        }
+                        else
+                        {
+                            foundGarbageClose = true;
+                        }
+                    }
+                }
+
+                if (input[i] == '{')
+                {
+                    i = TraverseGroup(input, i, currentLevel + 1, ref currentScore);
+                }
+                i++;
+            }
+
+            return i;
+
+        }
+
+        public static int Part2(string input)
+        {
+            return -1;
+        }
+    }
+}
---
+++ Day9Tests/Day9Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9.Tests
+{
+    [TestClass()]
+    public class Day9Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            Assert.AreEqual(Day9.Part1("{}"), 1);
+            Assert.AreEqual(Day9.Part1("{{{}}}"), 6);
+            Assert.AreEqual(Day9.Part1("{{},{}}"), 5);
+            Assert.AreEqual(Day9.Part1("{{{},{},{{}}}}"), 16);
+            Assert.AreEqual(Day9.Part1("{<a>,<a>,<a>,<a>}"), 1);
+            Assert.AreEqual(Day9.Part1("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
+            Assert.AreEqual(Day9.Part1("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
+            Assert.AreEqual(Day9.Part1("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
+        }
+
+        [TestMethod()]
+        public void Part2Test()
+        {
+            Assert.AreEqual(Day9.Part2("<>"), 0);
+            Assert.AreEqual(Day9.Part2("<random characters>"), 17);
+            Assert.AreEqual(Day9.Part2("<<<<>"), 3);
+            Assert.AreEqual(Day9.Part2("<{!>}>"), 2);
+            Assert.AreEqual(Day9.Part2("<!!>"), 0);
+            Assert.AreEqual(Day9.Part2("<!!!>>"), 0);
+            Assert.AreEqual(Day9.Part2("<{o\"i!a,<{ i < a > "), 10);
+        }
+
+    }
+}
